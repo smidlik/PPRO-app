aa466302b0cf79ccccbd2c587183b04994533a11
Jakub Šmída <43535798+smidlik@users.noreply.github.com>
2022-11-23 21:47:43 +0100

    Initial commit

---
+++ src/main/java/com/example/pproapp/PproAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.pproapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PproAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PproAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pproapp/PproAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pproapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PproAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
